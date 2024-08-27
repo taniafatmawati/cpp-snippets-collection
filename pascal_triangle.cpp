f41d3bc9b8dc60c53b7c2eb56caf5b1c24df01e6
Tania Fatma Wati <tania.fatmawati20@gmail.com>
2024-08-27 11:41:20 +0700

    Update pascal_triangle.cpp

--- pascal_triangle.cpp
+++ pascal_triangle.cpp
@@ -5,24 +5,16 @@ void printLine(int n);
 float add(float a, float b);
 long int factorial(int n);
 long int combination(int a, int b);
+void displayPascalTriangle(int n);
 
 int main()
 {
-    int n, i, j;
+    int n;
     cout << "Displaying Pascal's Triangle" << endl;
     printLine(28);
     cout << endl;
     cout << "Enter the depth of Pascal's Triangle n: "; cin >> n;
-    
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j <= i; j++)
-        {
-            cout << combination(i, j) << " ";
-        }
-        cout << endl;
-    }
-
+    displayPascalTriangle(n);
     getch();
 }
 
@@ -56,3 +48,25 @@ long int combination(int a, int b)
 {
     return (factorial(a) / (factorial(b) * factorial(a - b)));
 }
+
+void displayPascalTriangle(int n)
+{
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            cout << combination(i, j) << " ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+
+1   1               combination(1,0)  combination(1,1)
+1   2   1           combination(2,0)  combination(2,1)  combination(2,2)
+1   3   3   1       combination(3,0)  combination(3,1)  combination(3,2)  combination(3,3)
+1   4   6   4   1   combination(4,0)  combination(4,1)  combination(4,2)  combination(4,3)  combination(4,4)
+
+*/
